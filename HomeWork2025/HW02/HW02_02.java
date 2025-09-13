43ad9c02a7111b42e8cb0d335738b868836e5b56
Egor Orlik <egororlik27@knu.ua>
2025-09-13 18:38:47 +0300

    HomeWork_2

---
+++ HomeWork2025/HW02/HW02_02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HW02_02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number (0..65535): ");
+        int num = scan.nextInt();   // 2-байтовое число (0..65535)
+
+        int count = 0;
+        for (int i = 0; i < 16; i++) {
+            if ((num & 1) == 0) { // проверяем младший бит
+                count++;
+            }
+            num >>= 1; // сдвигаем вправо
+        }
+
+        System.out.println("Quantity 0 bite: " + count);
+    }
+}
