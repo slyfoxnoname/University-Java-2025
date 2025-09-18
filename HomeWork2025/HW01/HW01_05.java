8439f2310dec931949953717fd80dc4b3f6b67bb
Egor Orlik <egororlik27@knu.ua>
2025-09-18 16:28:27 +0300

    #1 Homework 05 task

---
+++ HomeWork2025/HW01/HW01_05.java
@@ -0,0 +1,22 @@
+public class HW01_05 {
+    public static void main(String[] args) {
+        int N, M;
+
+        if (args.length == 2) {
+            N = Integer.parseInt(args[0]);
+            M = Integer.parseInt(args[1]);
+        } else {
+            // якщо аргументів нема — зчитуємо з консолі
+            java.util.Scanner scan = new java.util.Scanner(System.in);
+            System.out.print("Введите N: ");
+            N = scan.nextInt();
+            System.out.print("Введите M: ");
+            M = scan.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            int random = (int)(Math.random() * (M + 1)); // от 0 до M включно
+            System.out.println(random);
+        }
+    }
+}
