05152901709d59e8db8f7854148c23b57db2a97b
Egor Orlik <egororlik27@knu.ua>
2025-09-30 18:23:17 +0300

    Complite HW02_03

--- HomeWork2025/HW02/HW02_03.java
+++ HomeWork2025/HW02/HW02_03.java
@@ -1,11 +1,32 @@
+public class HW02_03 {
+    public static double seriesSum(double x, double eps) {
+        // все ряды сходятся только если |x|<1
+        double fx = 0.5 * Math.log((1 + x) / (1 - x));
 
-import java.util.Scanner;
+        double sum = 0; 
+        double pow = x; 
+        int k = 0;
 
-public class HW02_03{
+        while (true) {
+            double term = pow / (2 * k + 1);
+
+            if (Math.abs(term) <= eps) {
+                break;
+            }
+
+            sum = sum + term;
+
+            pow = pow * x * x; 
+            k = k + 1;
+        }
+
+        return fx - sum;
+    }
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int x = scan.nextInt();
-        
+        double x = 0.7;
+        double eps = 0.0001;
+        double result = seriesSum(x, eps);
+        System.out.println("Result = " + result);
     }
 }
